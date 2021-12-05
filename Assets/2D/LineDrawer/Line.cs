7271a6bb87a7a0dde650aa1620eb616c9ba0887f
ZhiqiangYang <zhiqiang.yang@cootek.cn>
2021-12-05 19:13:16 +0800

    1、LineDrawer

---
+++ Assets/2D/LineDrawer/Line.cs
@@ -0,0 +1,84 @@
+﻿//------------------------------------------------------------
+// File : Line.cs
+// Email: mailto:zhiqiang.yang@cootek.cn
+// Desc : 
+//------------------------------------------------------------
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace GBGame
+{
+    public class Line : MonoBehaviour
+    {
+        public LineRenderer lineRenderer;
+        public EdgeCollider2D edgeCollider;
+        public Rigidbody2D rigidBody;
+
+        [HideInInspector] public List<Vector2> points = new List<Vector2>();
+        [HideInInspector] public int pointCount = 0;
+
+        float pointsMinDistance = 0.1f;
+        float circleColliderRadius;
+
+
+        public void AddPoint(Vector2 newPoint)
+        {
+            if (pointCount >= 1 && Vector2.Distance(newPoint, GetLastPoint()) < pointsMinDistance)
+                return;
+
+
+            points.Add(newPoint);
+            pointCount++;
+
+            // 添加圆形碰撞体
+            var circleCollider = this.gameObject.AddComponent<CircleCollider2D>();
+            circleCollider.offset = newPoint;
+            circleCollider.radius = circleColliderRadius;
+
+
+            lineRenderer.positionCount = pointCount;
+            lineRenderer.SetPosition(pointCount - 1, newPoint);
+
+            if (pointCount > 1)
+            {
+                edgeCollider.points = points.ToArray();
+            }
+
+        }
+
+        public Vector2 GetLastPoint()
+        {
+            return lineRenderer.GetPosition(pointCount - 1);
+        }
+
+        public void UsePhysics(bool usePhysics)
+        {
+            rigidBody.isKinematic = !usePhysics;
+        }
+
+
+        public void SetLineColor(Gradient colorGradient)
+        {
+            lineRenderer.colorGradient = colorGradient;
+        }
+
+        public void SetPointsMinDistance(float distance)
+        {
+            pointsMinDistance = distance;
+        }
+
+        public void SetLineWidth(float width)
+        {
+            lineRenderer.startWidth = width;
+            lineRenderer.endWidth = width;
+
+            circleColliderRadius = width / 2;
+
+            edgeCollider.edgeRadius = circleColliderRadius;
+        }
+    }
+}
+
+
---
+++ Assets/2D/LineDrawer/LineDrawer.cs
@@ -0,0 +1,99 @@
+﻿//------------------------------------------------------------
+// File : LineDrawer.cs
+// Email: mailto:zhiqiang.yang@cootek.cn
+// Desc : 
+//------------------------------------------------------------
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GBGame
+{
+    public class LineDrawer : MonoBehaviour
+    {
+        public GameObject linePrefab;
+
+        public LayerMask canDrawOverLayer;
+        int canDrawLayerIndex;
+
+        [Space(30)]
+        public Gradient lineColor;
+        public float linePointsMinDistance;
+        public float lineWidth;
+
+        Line currentLine;
+        Camera cam;
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            cam = Camera.main;
+            canDrawLayerIndex = LayerMask.NameToLayer("CantDrawOver");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+                BeginDraw();
+            if (null != currentLine)
+                Draw();
+            if (Input.GetMouseButtonUp(0))
+                EndDraw();
+        }
+
+
+        // 画线逻辑
+
+        void BeginDraw()
+        {
+            currentLine = Instantiate(linePrefab, this.transform).GetComponent<Line>();
+
+            currentLine.UsePhysics(false);
+            currentLine.SetLineColor(lineColor);
+            currentLine.SetPointsMinDistance(linePointsMinDistance);
+            currentLine.SetLineWidth(lineWidth);
+
+
+        }
+
+        void Draw()
+        {
+            var pos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+            RaycastHit2D hit = Physics2D.CircleCast(pos, lineWidth / 3f, Vector2.zero, 1f, canDrawOverLayer);
+            if (hit)
+            {
+                EndDraw();
+            }
+            else
+            {
+                currentLine.AddPoint(pos);
+            }
+
+        }
+
+        void EndDraw()
+        {
+            if (currentLine == null)
+            {
+                return;
+            }
+
+            if (currentLine.pointCount < 2)
+            {
+                Destroy(currentLine.gameObject);
+                currentLine = null;
+            }
+            else
+            {
+                currentLine.gameObject.layer = canDrawLayerIndex;
+                currentLine.UsePhysics(true);
+                currentLine = null;
+            }
+        }
+    }
+}
+
+
